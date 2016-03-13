726dd9032461c168ec997505b3d11b7d36a4a238
ksmithut <ksmith.ut@gmail.com>
2016-03-12 22:41:13 -0700

    more work on grabbing config from eslint.org

--- bin/generate.js
+++ bin/generate.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+const path = require('path')
+const fs = require('fs')
+const Promise = require('bluebird')
 const generate = require('../lib/generate')
 
+const FILE_PATH = path.resolve(__dirname, '..', '.eslintrc.js')
+
+Promise.promisifyAll(fs)
+
 generate()
+  .then((str) => fs.writeFileAsync(FILE_PATH, str))
+  .then(() => console.log('done'))
