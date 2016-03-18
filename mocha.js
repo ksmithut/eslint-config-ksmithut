3f1fb9d2142b90a60145514b3d212b6625bdf3f5
ksmithut <ksmith.ut@gmail.com>
2016-03-18 10:18:51 -0600

    disables sync tests rules

--- mocha.js
+++ mocha.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'mocha/no-exclusive-tests': 2, // Prevent .only tests
     'mocha/handle-done-callback': 2, // Handle the done callback
-    'mocha/no-synchronous-tests': 2, // Disallow Synchronous Tests (when done is passed in)
+    'mocha/no-synchronous-tests': 0, // Allow Synchronous tests
     'mocha/no-global-tests': 2 // Disallow global tests
   }
 }
